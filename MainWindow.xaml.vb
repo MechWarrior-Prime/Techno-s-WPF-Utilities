d094e482106e68e94410252cc838b2e93dede3ef
Frank Dahncke <MechWarrior@gmx.com>
2020-05-12 16:18:40 +0200

    Add project files.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class MainWindow
+
+    Private Sub tbOutput_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles tbOutput.MouseDown
+        tbOutput.Text = e.ChangedButton.ToString
+    End Sub
+
+End Class
